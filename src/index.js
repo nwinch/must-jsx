a40f477992a87d8da744c3a051a6b0ff614e66fc
Nathan Winch <nwinch@gmail.com>
2016-04-30 10:09:40 +1000

    Pass transformed as expected + actual

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import ReactElementToJSXString from "react-element-to-jsx-string"
 
-export default function(Must, custom) {
-  const opts = custom ? { isElement: custom } : {}
-
+export default function(Must) {
   Must.prototype.jsx = function(expected) {
-    const actual = ReactElementToJSXString(this.actual, opts)
-    const transformed = ReactElementToJSXString(expected, opts)
+    const actual = ReactElementToJSXString(this.actual)
+    const transformed = ReactElementToJSXString(expected)
+    const opts = { actual, expected: transformed }
 
-    this.assert(actual === transformed, "equal JSX",  { expected: expected })
+    this.assert(actual === transformed, "equal JSX", opts)
+    return this
   }
 }
